935f78cf55b213b096edfec8ab769bafdd7757bd
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-08-25 19:20:35 +0300

    append all customers

--- lib/Customer.py
+++ lib/Customer.py
@@ -4,5 +4,8 @@ class Customer:
     def __init__(self, given_name,family_name):
         self.given_name = given_name
         self.family_name = family_name
+        Customer.all_customers.append(self)
+
+    
 
         
