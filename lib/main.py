7173ed11ec540c77602c7560cdc573a07095aadf
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-08-25 21:38:41 +0300

    created customers and restaurants

--- lib/main.py
+++ lib/main.py
@@ -1,3 +1,11 @@
 from Customer import Customer
 from Restaurant import Restaurant
 
+# create customers
+customer1 = Customer("Linda", "Wambui")
+customer2 = Customer("Ali", "Malik")
+
+# create restaurants
+restaurant1 = Restaurant("Dusit Princess")
+restaurant2 = Restaurant("Delicious Eatery")
+
