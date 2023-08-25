5e74016b5e474338ed11cc72e7d375b8db059e59
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-08-25 20:38:04 +0300

    return all the reviews

--- lib/Review.py
+++ lib/Review.py
@@ -1,11 +1,13 @@
 class Review:
+    all_reviews = []
 
     def __init__(self,customer,restaurant,rating):
         self.customer = customer
         self.restaurant = restaurant
         self.rating = rating
+        Review.all_reviews.append(self)
 
     def rating(self):
         return self.rating    
 
-   
+
