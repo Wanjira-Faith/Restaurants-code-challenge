33384c284ebdfc8844b4a074365d8bb4ada2b8ca
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-08-25 21:30:43 +0300

    added customer and restaurant method

--- lib/Review.py
+++ lib/Review.py
@@ -8,6 +8,10 @@ class Review:
         Review.all_reviews.append(self)
 
     def rating(self):
-        return self.rating    
-
-
+        return self.rating 
+    
+    def customer(self):
+        return self.customer
+    
+    def restaurant(self):
+        return self.restaurant
