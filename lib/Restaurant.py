225a3e6218a4ef1cc6b5f7bad024e684b4a378a6
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-08-25 20:14:54 +0300

    returns a list of all customers who have reviewed a particular restaurant

--- lib/Restaurant.py
+++ lib/Restaurant.py
@@ -1,7 +1,9 @@
 class Restaurant:
+    all_restaurants = []
 
     def __init__(self, name):
         self.name = name
         self.reviews = []
-        
+        Restaurant.all_restaurants.append(self)
+
 
