493673e691f0c685e670fa06d730a5f52fee0fd5
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-08-25 19:22:35 +0300

    define given name

--- lib/Customer.py
+++ lib/Customer.py
@@ -6,6 +6,7 @@ class Customer:
         self.family_name = family_name
         Customer.all_customers.append(self)
 
-    
+    def given_name(self,name):
+        self.given_name = name
 
         
