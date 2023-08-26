5f0e2cb9bd0ad59c941dbc231fabe3719ccccd14
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-08-26 15:33:39 +0300

    set customers and restaurants as private attributes to avoid being modified directly outside the class

--- lib/Review.py
+++ lib/Review.py
@@ -2,8 +2,8 @@ class Review:
     all_reviews = []
 
     def __init__(self,customer,restaurant,rating):
-        self.customer = customer
-        self.restaurant = restaurant
+        self._customer = customer
+        self._restaurant = restaurant
         self.rating = rating
         Review.all_reviews.append(self)
 
