d3ecd61616b9b4164eb1ba46c66d4e500c61a3e7
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-08-26 11:42:25 +0300

    added a reviews attribute

--- lib/Customer.py
+++ lib/Customer.py
@@ -6,6 +6,7 @@ class Customer:
     def __init__(self, given_name,family_name):
         self.given_name = given_name
         self.family_name = family_name
+        self.reviews = []
         Customer.all_customers.append(self)
 
     def given_name(self):
